688940ea573ae5e007c504a108609bfc3ba49f8d
reece72 <102812056+reece72@users.noreply.github.com>
2022-04-08 14:49:33 +0100

    Add files via upload

---
+++ topics_export_2022-04-08_144802.sql
@@ -0,0 +1,2 @@
+insert into `topics` (`created_at`, `id`, `name`) values ('2022-04-07 12:21:42', 1, 'Topic1');
+insert into `topics` (`created_at`, `id`, `name`) values ('2022-04-07 12:21:59', 2, 'topic2');
